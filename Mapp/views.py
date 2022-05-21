ce49586aca2d622daac5908b1286302e20a9797b
Sam231221 <rasengankid999@gmail.com>
2022-05-21 18:04:31 +0545

    Project updated

--- Mapp/views.py
+++ Mapp/views.py
@@ -3,6 +3,14 @@ from django.views.generic import View
 
 from Mapp.models import Resource
 from django.db.models import Q  # New
+'''
+we use Q objects to make more complex queries following
+
+Use & (AND) operator to search for more than 2 fields. For example, when searching title & content, both should be true, when searching for a word such as “python”, so it(python) should be contained in both title and content fields
+Use | (OR) operator to search for only one field. For example, when searching title | content, both don’t have to be true, only one is okay, when searching for a word such as “python”, so it(python) doesn’t have to be contained in both title and content fields
+'''
+
+
 class HomeView(View):
  def get(self, request):
     context={}
