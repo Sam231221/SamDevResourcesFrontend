be61402a57863d9acb9b64c4e4c779c5a9527a2b
Sam231221 <rasengankid999@gmail.com>
2022-06-06 14:49:39 +0545

    project updated

--- AConfig/settings.py
+++ AConfig/settings.py
@@ -103,11 +103,19 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+# meaning- static url is /static/ which is a relative path and appears in url section .we dont need to mention statiic path in codes(links)
+STATIC_URL = '/staticfiles/'
 
-STATIC_URL = 'static/'
+# MUST FOR LOADIN STATIC FILES
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# WHITENOISE CONFIGURATION FOR SERVING STATIC FILES ONLY.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
