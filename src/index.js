0f24dfae373bdcd5f083c0c8f0c972000fccdb73
Sam231221 <rasengankid999@gmail.com>
2022-09-20 20:17:43 +0545

    Deployment Configuration

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-
 import App from './App';
+
+import axios from 'axios'
+
+axios.defaults.baseURL=process.env.REACT_APP_API
 
 ReactDOM.render(
     <App />,
